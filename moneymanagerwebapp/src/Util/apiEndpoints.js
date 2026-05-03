940156922985db2896acb9148e572403b5f92c9a
shan2302 <shantanu230205@gmail.com>
2026-05-03 17:10:11 +0530

    ProdIssueSolving

--- moneymanagerwebapp/src/Util/apiEndpoints.js
+++ moneymanagerwebapp/src/Util/apiEndpoints.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "https://moneymanager-z7tl.onrender.com/api/v1.0";
+// export const BASE_URL = "https://moneymanager-z7tl.onrender.com/api/v1.0";
+export const BASE_URL = "http://localhost:8080/api/v1.0"
 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
--- src/main/java/com/shantanu/moneymanager/service/EmailService.java
+++ src/main/java/com/shantanu/moneymanager/service/EmailService.java
@@ -1,12 +1,14 @@
 package com.shantanu.moneymanager.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailService {
@@ -17,6 +19,11 @@ public class EmailService {
 
     @Async
     public void sendEmail(String to, String subject, String body) {
+        if (mailSender instanceof org.springframework.mail.javamail.JavaMailSenderImpl) {
+            org.springframework.mail.javamail.JavaMailSenderImpl impl =
+                    (org.springframework.mail.javamail.JavaMailSenderImpl) mailSender;
+            System.out.println("Attempting connection to: " + impl.getHost() + " on port: " + impl.getPort());
+        }
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(fromEmail);
@@ -27,7 +34,6 @@ public class EmailService {
             System.out.println("Email sent successfully to: " + to);
         }catch(Exception e ){
             throw new RuntimeException(e.getMessage());
-            System.err.println("FAILED to send email: " + e.getMessage());
         }
     }
 }
