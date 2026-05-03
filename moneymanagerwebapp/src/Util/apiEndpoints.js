29aaf78db413064a9b9733a5f776147aa9c4d8d5
shan2302 <shantanu230205@gmail.com>
2026-05-03 17:26:26 +0530

    Issue of 587 port to 465

--- moneymanagerwebapp/src/Util/apiEndpoints.js
+++ moneymanagerwebapp/src/Util/apiEndpoints.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://moneymanager-z7tl.onrender.com/api/v1.0";
-export const BASE_URL = "http://localhost:8080/api/v1.0"
+export const BASE_URL = "https://moneymanager-z7tl.onrender.com/api/v1.0";
+// export const BASE_URL = "https://localhost:8080/api/v1.0"
 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
